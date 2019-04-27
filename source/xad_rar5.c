71ae0b6a3d3c8cdddf9a99083c1e59b0a22ce673
Chris Young <chris@unsatisfactorysoftware.co.uk>
2019-04-27 21:52:44 +0100

    RAR5 client

--- source/xad_rar5.c
+++ source/xad_rar5.c
@@ -1,4 +1,4 @@
-/* XAD RAR client
+/* XAD RAR5 client
  * by Chris Young <chris@unsatisfactorysoftware.co.uk>
  */
 
@@ -9,7 +9,7 @@
 #include "macros.h"
 
 #ifndef XADMASTERFILE
-#define rar_Client		FirstClient
+#define rar5_Client		FirstClient
 #define NEXTCLIENT		0
 #ifdef __amigaos4__
 #define XADMASTERVERSION	13
@@ -19,17 +19,17 @@
 const char *version = VERSTAG;
 #endif
 
-XAD_MACRO_RECOGFILE(rar)
+XAD_MACRO_RECOGFILE(rar5)
 {
-  if((data[0]=='R') & (data[1]=='a') & (data[2]=='r') & (data[3]=='!') & (data[4]==0x1A) & (data[5]==0x07) & (data[6]==0x00)) {
+  if((data[0]=='R') & (data[1]=='a') & (data[2]=='r') & (data[3]=='!') & (data[4]==0x1A) & (data[5]==0x07) & (data[6]==0x01) & (data[7]==0x00)) {
 		return 1; /* known file */
   }
 
     return 0; /* unknown file */
 }
 
-XAD_MACRO_GETINFO(rar)
-XAD_MACRO_UNARCHIVE(rar)
-XAD_MACRO_CLIENT(rar, "RAR", 0)
+XAD_MACRO_GETINFO(rar5)
+XAD_MACRO_UNARCHIVE(rar5)
+XAD_MACRO_CLIENT(rar5, "RAR5", 0)
 
 #endif /* XADMASTER_RAR_C */
